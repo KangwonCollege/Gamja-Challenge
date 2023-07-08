918a211334903eed2070911388c9f721950f5cf2
gunyu1019 <gunyu1019@gmail.com>
2023-07-09 01:11:28 +0900

    [feat] add requirements(aiofiles) and using `configparser.get`

--- main.py
+++ main.py
@@ -7,16 +7,13 @@ from utils.getConfig import get_config
 
 if __name__ == "__main__":
     directory = os.path.dirname(os.path.abspath(__file__))
-    
-    if False:
-        pass
-    else:
-        bot = interaction.Client(
-            intents = discord.Intents.default(),
-            enable_debug_events=True,
-            global_sync_command=True
-        )
-        
+    parser = get_config()
+
+    bot = interaction.Client(
+        intents=discord.Intents.default(),
+        enable_debug_events=True,
+        global_sync_command=False
+    )
+
     bot.load_extensions("cogs", directory=directory)
-    bot.run(get_config()["TOKEN"]["discord_token"])
-    
+    bot.run(parser.get("TOKEN", "discord_token"))
