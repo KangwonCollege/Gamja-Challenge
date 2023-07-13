c9e1c42cef39b8a19616b2902b0772e1e929cad4
gunyu1019 <gunyu1019@gmail.com>
2023-07-13 22:33:50 +0900

    [bug] sqlalchemy url

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import os
 
 import discord
+import sqlalchemy
 from discord.ext import interaction
 from sqlalchemy import NullPool
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy import MetaData
+from sqlalchemy.ext.asyncio import AsyncConnection
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine
 
+from models import database as db_model
 from utils.getConfig import get_config
+
 
 if __name__ == "__main__":
     directory = os.path.dirname(os.path.abspath(__file__))
@@ -23,19 +28,21 @@ if __name__ == "__main__":
     database_parser = get_config("database")
     database_section = database_parser.get("Default", "database_section")
     database = {
+        "drivername": "mysql+aiomysql",
         "username": database_parser.get(database_section, "user"),
         "host": database_parser.get(database_section, "host"),
         "password": database_parser.get(database_section, "password"),
         "database": database_parser.get(database_section, "database"),
         "port": database_parser.getint(database_section, "port", fallback=3306),
     }
+    database_url = sqlalchemy.engine.url.URL.create(
+        **database
+    )
     engine = create_async_engine(
-        "mysql+aiomysql://{username}:{password}@{host}:{port}/{database}".format(
-            **database
-        ),
+        database_url,
         poolclass=NullPool,
     )
-    factory = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+    factory = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
     bot.load_extensions("cogs", directory=directory, factory=factory)
     bot.run(parser.get("TOKEN", "discord_token"))
