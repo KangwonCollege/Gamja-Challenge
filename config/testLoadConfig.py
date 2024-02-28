3289d609a54364aed9660def74bb23946af3a237
Mule129 <mule01129@gmail.com>
2024-02-29 02:49:29 +0900

    [Feat] #3 - Del GitUserProfile file, test file

--- config/testLoadConfig.py
+++
@@ -1,6 +0,0 @@
-import configparser
-
-config = configparser.ConfigParser()
-print(config.read(r"C:\Users\Mule129\Documents\GitHub\Gamja-Challenge\config\token.ini"))
-print(config.sections())
-print(config["TOKEN"]["git_token"])
